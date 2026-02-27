a9466cc9b5ec603cc0e1ffa84b7a6287c9a0af51
Michal <terox@wp.pl>
2026-02-27 23:53:39 +0100

    feat: add getLegoSets

--- src/pages/LegoSets.jsx
+++ src/pages/LegoSets.jsx
@@ -1,8 +1,71 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
 function LegoSets() {
+    const [sets, setSets] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const cellStyle = {
+        border: "1px solid #ddd",
+        padding: "8px"
+    };
+
+    useEffect(() => {
+        fetchSets();
+    }, []);
+
+    const fetchSets = async () => {
+        try {
+            const response = await api.get("/api/lego-sets");
+            setSets(response.data);
+        } catch (error) {
+            console.error("Error fetching lego sets", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    if (loading) return <p>Loading...</p>;
+
     return (
         <div>
             <h2>Lego Sets</h2>
-            <p>Logged in successfully ✅</p>
+
+            {sets.length === 0 ? (
+                <p>No lego sets found.</p>
+            ) : (
+                <table style={{ width: "100%", borderCollapse: "collapse" }}>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Set Number</th>
+                        <th>Pieces</th>
+                        <th>Warehouse</th>
+                        <th>Store</th>
+                        <th>Price</th>
+                        <th>Series</th>
+                        <th>Condition</th>
+                        <th>Availability</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {sets.map((set) => (
+                        <tr key={set.id}>
+                            <td>{set.id}</td>
+                            <td style={cellStyle}>{set.name}</td>
+                            <td>{set.setNumber}</td>
+                            <td>{set.numberOfPieces}</td>
+                            <td>{set.quantityInWarehouse}</td>
+                            <td>{set.quantityInStore}</td>
+                            <td>{set.basePrice}</td>
+                            <td>{set.series}</td>
+                            <td>{set.condition}</td>
+                            <td>{set.availabilityType}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 }
